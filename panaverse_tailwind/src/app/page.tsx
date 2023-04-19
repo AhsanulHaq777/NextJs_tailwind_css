11f572dbf9bb860bdece83cd5d4ca8e8f04d62ef
ahsanulhaq <ulhaq.ahsan777@gmail.com>
2023-04-19 17:47:56 +0500

    Created new project and configured tailwind css

---
+++ panaverse_tailwind/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+      )
+}
